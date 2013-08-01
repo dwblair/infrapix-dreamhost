5f819cdac2a13e2b4f73bc24615b09acc26e879e
dwblair <donblair@gmail.com>
2013-08-01 15:00:10 -0400

    fixed color range

--- app/views.py
+++ app/views.py
@@ -48,109 +48,111 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
 
 def nir(imageInPath,imageOutPath):
-	img=Image.open(imageInPath)
-	imgR, imgG, imgB = img.split() #get channels
-	arrR = numpy.asarray(imgR).astype('float64')
+    img=Image.open(imageInPath)
+    imgR, imgG, imgB = img.split() #get channels
+    arrR = numpy.asarray(imgR).astype('float64')
 
-	arr_nir=arrR
+    arr_nir=arrR
 
-	img_w,img_h=img.size
+    img_w,img_h=img.size
 
-	dpi=600. #need this to be floating point!
-	fig_w=img_w/dpi
-	fig_h=img_h/dpi
+    dpi=600. #need this to be floating point!
+    fig_w=img_w/dpi
+    fig_h=img_h/dpi
 
-	fig=plt.figure(figsize=(fig_w,fig_h),dpi=dpi)
+    fig=plt.figure(figsize=(fig_w,fig_h),dpi=dpi)
 
-	fig.set_frameon(False)
+    fig.set_frameon(False)
 
-	ax_rect = [0.0, #left
-	       0.0, #bottom
-	       1.0, #width
-	       1.0] #height
-	ax = fig.add_axes(ax_rect)
-	ax.yaxis.set_ticklabels([])
-	ax.xaxis.set_ticklabels([])   
-	ax.set_axis_off()
-	ax.axes.get_yaxis().set_visible(False)
-	ax.patch.set_alpha(0.0)
+    ax_rect = [0.0, #left
+           0.0, #bottom
+           1.0, #width
+           1.0] #height
+    ax = fig.add_axes(ax_rect)
+    ax.yaxis.set_ticklabels([])
+    ax.xaxis.set_ticklabels([])   
+    ax.set_axis_off()
+    ax.axes.get_yaxis().set_visible(False)
+    ax.patch.set_alpha(0.0)
 
-	axes_img = ax.imshow(arr_nir,
-			  cmap=plt.cm.gist_gray,
-			  aspect = 'equal',
-			  interpolation="nearest"
-			 )
+    axes_img = ax.imshow(arr_nir,
+              cmap=plt.cm.gist_gray,
+              aspect = 'equal',
+              interpolation="nearest"
+             )
 
-	fig.savefig(imageOutPath, 
-		    dpi=dpi,
-		    bbox_inches='tight',
-		    pad_inches=0.0, 
-		   )
+    fig.savefig(imageOutPath, 
+            dpi=dpi,
+            bbox_inches='tight',
+            pad_inches=0.0, 
+           )
 
-	#plt.show()  #show the plot after saving
-	fig.clf()
-	plt.close()
-	gc.collect()
+    #plt.show()  #show the plot after saving
+    fig.clf()
+    plt.close()
+    gc.collect()
 
 
 
 def ndvi(imageInPath,imageOutPath):
-	img=Image.open(imageInPath)
-	imgR, imgG, imgB = img.split() #get channels
-	arrR = numpy.asarray(imgR).astype('float64')
-	arrB = numpy.asarray(imgB).astype('float64')
-	
-	num=arrR - arrB
-	num=(arrR - arrB)
-	denom=(arrR + arrB)
-	arr_ndvi=num/denom
+    img=Image.open(imageInPath)
+    imgR, imgG, imgB = img.split() #get channels
+    arrR = numpy.asarray(imgR).astype('float64')
+    arrB = numpy.asarray(imgB).astype('float64')
+    
+    num=arrR - arrB
+    num=(arrR - arrB)
+    denom=(arrR + arrB)
+    arr_ndvi=num/denom
 
-	vmin=-1.
-	vmax = 1.
+    vmin=-1.
+    vmax = 1.
 
-	img_w,img_h=img.size
+    img_w,img_h=img.size
 
-	dpi=600. #need this to be floating point!
-	fig_w=img_w/dpi
-	fig_h=img_h/dpi
+    dpi=600. #need this to be floating point!
+    fig_w=img_w/dpi
+    fig_h=img_h/dpi
 
-	fig=plt.figure(figsize=(fig_w,fig_h),dpi=dpi)
+    fig=plt.figure(figsize=(fig_w,fig_h),dpi=dpi)
 
-	fig.set_frameon(False)
+    fig.set_frameon(False)
 
-	ax_rect = [0.0, #left
-	       0.0, #bottom
-	       1.0, #width
-	       1.0] #height
-	ax = fig.add_axes(ax_rect)
-	ax.yaxis.set_ticklabels([])
-	ax.xaxis.set_ticklabels([])   
-	ax.set_axis_off()
-	ax.axes.get_yaxis().set_visible(False)
-	ax.patch.set_alpha(0.0)
+    ax_rect = [0.0, #left
+           0.0, #bottom
+           1.0, #width
+           1.0] #height
+    ax = fig.add_axes(ax_rect)
+    ax.yaxis.set_ticklabels([])
+    ax.xaxis.set_ticklabels([])   
+    ax.set_axis_off()
+    ax.axes.get_yaxis().set_visible(False)
+    ax.patch.set_alpha(0.0)
 
-	axes_img = ax.imshow(arr_ndvi,
-			  cmap=plt.cm.spectral, 
-			  aspect = 'equal',
-			  interpolation="nearest"
-			 )
+    axes_img = ax.imshow(arr_ndvi,
+              cmap=plt.cm.spectral, 
+              aspect = 'equal',
+              interpolation="nearest",
+              vmin=-1.0,
+              vmax=1.0
+             )
 
-	# Add colorbar 
-	#make an axis for colorbar
-	cax = fig.add_axes([0.8,0.05,0.05,0.85]) #left, bottom, width, height
-	cbar = fig.colorbar(axes_img, cax=cax)  #this resizes the axis
-	cbar.ax.tick_params(labelsize=2) #this changes the font size on the axis
+    # Add colorbar 
+    #make an axis for colorbar
+    cax = fig.add_axes([0.8,0.05,0.05,0.85]) #left, bottom, width, height
+    cbar = fig.colorbar(axes_img, cax=cax)  #this resizes the axis
+    cbar.ax.tick_params(labelsize=2) #this changes the font size on the axis
 
-	fig.savefig(imageOutPath, 
-		    dpi=dpi,
-		    bbox_inches='tight',
-		    pad_inches=0.0, 
-		   )
+    fig.savefig(imageOutPath, 
+            dpi=dpi,
+            bbox_inches='tight',
+            pad_inches=0.0, 
+           )
 
-	#plt.show()  #show the plot after saving
-	fig.clf()
-	plt.close()
-	gc.collect()
+    #plt.show()  #show the plot after saving
+    fig.clf()
+    plt.close()
+    gc.collect()
 
 
 def allowed_file(filename):
@@ -159,18 +161,18 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-	if request.method=='POST':
-		file=request.files['file']
-		if file and allowed_file(file.filename):
-			filename=secure_filename(file.filename)
-			uploadFilePath=os.path.join(app.config['UPLOAD_FOLDER'],filename)
-			file.save(uploadFilePath)
-			ndviFilePath=os.path.join(app.config['UPLOAD_FOLDER'],'ndvi_'+filename)
-			nirFilePath=os.path.join(app.config['UPLOAD_FOLDER'],'nir_'+filename)
-			ndvi(uploadFilePath,ndviFilePath)
-			nir(uploadFilePath,nirFilePath)
-			return redirect(url_for('uploaded_file',filename=filename)) 
-	return render_template('index.html')
+    if request.method=='POST':
+        file=request.files['file']
+        if file and allowed_file(file.filename):
+            filename=secure_filename(file.filename)
+            uploadFilePath=os.path.join(app.config['UPLOAD_FOLDER'],filename)
+            file.save(uploadFilePath)
+            ndviFilePath=os.path.join(app.config['UPLOAD_FOLDER'],'ndvi_'+filename)
+            nirFilePath=os.path.join(app.config['UPLOAD_FOLDER'],'nir_'+filename)
+            ndvi(uploadFilePath,ndviFilePath)
+            nir(uploadFilePath,nirFilePath)
+            return redirect(url_for('uploaded_file',filename=filename)) 
+    return render_template('index.html')
 
 @app.route('/dragdrop')
 def dragdrop():
